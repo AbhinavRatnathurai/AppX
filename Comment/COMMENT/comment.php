542372b696f743666aa607b903ac6c9967c01235
losSanJay <115866740+losSanJay@users.noreply.github.com>
2023-02-13 18:35:36 +0530

    new comment folder added

---
+++ Comment/COMMENT/comment.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- css file link  -->
+    <link rel="stylesheet" href="mycomment.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="title">Comment Form</div>
+      <div class="content">
+        <form name="commentf" action="" method="post">
+          <span class="details">Add your comment hear</span>
+          <textarea id="comment" name="comment" id="comment"> </textarea>
+          <input type="submit" />
+          <input type="reset" name="reset" />
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
+
+
+
+
+ 
+
+
