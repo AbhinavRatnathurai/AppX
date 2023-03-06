23dbcf8a67484e883a6c4687ec1ba0e0803ff5c5
losSanJay <115866740+losSanJay@users.noreply.github.com>
2023-03-07 01:42:36 +0530

    new test file

--- test/test_model.py
+++ test/test_model.py
@@ -1,60 +1,33 @@
 import unittest
+import sys
+
+sys.path.insert(0, "C:\\Users\\user\\Desktop\\AppX-Front-End\\Python_backend")
 
 from model import preprocess
 
+
 class TestPreprocess(unittest.TestCase):
-    
-    def test_preprocess(self):
-        text = "Hello! This is a sample text with numbers 123 and special characters @$%."
-        expected_output = "hello. this is a sample text with numbers and special characters."
+
+    def test_clean_text(self):
+        # Test that the function removes special characters and numbers
+        text = "This is a test! 1234 #$%&"
+        expected_output = "this is a test!"
         self.assertEqual(preprocess(text), expected_output)
-        
-    def test_preprocess_with_empty_string(self):
+
+    def test_remove_spaces(self):
+        # Test that the function removes unwanted spaces
+        text = "   This is a   test.  "
+        expected_output = "this is a test."
+        self.assertEqual(preprocess(text), expected_output)
+
+    def test_lowercase(self):
+        # Test that the function converts text to lowercase
+        text = "THIS IS A TEST."
+        expected_output = "this is a test."
+        self.assertEqual(preprocess(text), expected_output)
+
+    def test_empty_input(self):
+        # Test that the function handles empty input
         text = ""
         expected_output = ""
         self.assertEqual(preprocess(text), expected_output)
-        
-    def test_preprocess_with_only_special_characters(self):
-        text = "@$%!"
-        expected_output = ""
-        self.assertEqual(preprocess(text), expected_output)
-        
-    def test_preprocess_with_only_numbers(self):
-        text = "123456"
-        expected_output = ""
-        self.assertEqual(preprocess(text), expected_output)
-        
-    def test_preprocess_with_multiple_spaces(self):
-        text = "    hello   world     !"
-        expected_output = "hello. world."
-        self.assertEqual(preprocess(text), expected_output)
-        
-    def test_preprocess_with_newline_characters(self):
-        text = "Hello!\nThis is a sample text.\nWith newline characters."
-        expected_output = "hello. this is a sample text. with newline characters."
-        self.assertEqual(preprocess(text), expected_output)
-
-
-
-class TestCalculateStarRating(unittest.TestCase):
-    
-    def test_calculate_star_rating(self):
-        comm_db = "This product is amazing. I love it!"
-        expected_result = {"positive": 2, "negative": 0}
-        
-        # import the function to be tested
-        from test_model import calculate_star_rating
-        
-        # call the function with the test input
-        result = calculate_star_rating(comm_db)
-        
-        # assert that the result matches the expected output
-        self.assertEqual(result, expected_result)
-
-
-
-if __name__ == '__main__':
-    unittest.main()
-
-
-
