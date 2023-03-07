8971f93a381e0998affa021661e1b6e6c44644fa
RaviNdu2023 <ravindu.20211209@iit.ac.lk>
2023-03-07 20:51:38 +0530

    csrate updating successfully and visible in the frontend.

---
+++ Comment/star_gen.py
@@ -0,0 +1,162 @@
+import openai
+import re
+import time
+import mysql.connector
+import sys
+
+# Insert your API key here
+openai.api_key = "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"
+
+def preprocess(text):
+    try:
+        # Remove special characters and numbers
+        cleaned = re.sub('[^a-zA-Z.,!]', ' ', text)
+        cleaned = re.sub(r'\s+', ' ', cleaned)
+        # Remove unwanted spaces at the beginning and end of each sentence
+        sentences = cleaned.split(".")
+        cleaned = ".".join([sentence.strip() for sentence in sentences])
+        # Return the cleaned text
+        return cleaned.lower()
+    except Exception as e:
+        print("An error occured:", e)
+    
+
+def gpt_prompt(prompt):
+    while True:
+        try:
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.5,
+                best_of=1,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+            break
+        except openai.OpenAIError as e:
+            time.sleep(3)
+    return response
+
+
+def count_sentiment_words(sentence):
+        
+    response = gpt_prompt("Please analyze the following sentence and return only the numerical value of the count of words with positive connotations, including those with implicit positive meanings." + sentence)
+
+    if response:
+        positive_count = response["choices"][0]["text"].split("\n")
+        positive_count = int(positive_count[2])
+    else:
+        positive_count = None
+
+    response = gpt_prompt("Please analyze the following sentence and return only the numerical value of the count of words with negative connotations, including those with implicit negative meanings." + sentence)
+    
+    if response:
+        negative_count = response["choices"][0]["text"].split("\n")
+        negative_count = int(negative_count[2])
+    else:
+        negative_count = None
+
+    # Generate the overall sentence ranks
+    sentence_rank = round(calculate(positive_count, negative_count), 1)
+
+    # Update the database with the calculated star rating
+    #mycursor.execute("UPDATE comment SET star_rating = %s WHERE id = %s", (sentence_rank, cId))
+    #mydb.commit()
+
+    return sentence_rank
+
+
+def calculate(positive_count, negative_count):
+    try:
+        if positive_count + negative_count == 0:
+            return 0
+        ratio = positive_count / (positive_count + negative_count)
+        points = ratio * 5
+        return points
+    except ZeroDivisionError as e:
+        print("Error: Cannot divide by zero.")
+    except Exception as e:
+        print("Error: ", e)
+
+
+# Define the function to calculate the star rating for a comment
+def calculate_star_rating(comm_db):
+    # Send for text pre-processing
+    processed = preprocess(comm_db)
+
+    # Retrieve both positive words count and negative words count
+    result = count_sentiment_words(processed)
+    return result
+
+
+def main():
+
+    config = {
+    'user': 'root',
+    'password': '',
+    'host': 'localhost',
+    'database': 'appx',
+    'raise_on_warnings': True,
+    'port': '3306'
+    }
+    try:
+        # Connect to the database
+        cnx = mysql.connector.connect(**config)
+
+        # Perform some database operations
+        cursor = cnx.cursor()
+
+        comm = sys.argv[1]
+        product_id = sys.argv[2]
+        comment_id = sys.argv[3]
+
+        # print(comm,product_id,comment_id)
+        
+        star_rating = calculate_star_rating(comm)
+
+        # Update the comment with the star rating
+        cursor = cnx.cursor() # Rename mycursor to cursor
+        query = "UPDATE comment SET csRate = %s WHERE cId = %s AND pId = %s"
+        values = (star_rating, comment_id, product_id)
+        cursor.execute(query, values)
+        cnx.commit()
+
+        print("done")
+        # Pause briefly between batches
+        time.sleep(5)
+
+    except mysql.connector.Error as err:
+        print(f"Error connecting to the database: {err}")
+    except Exception as e:
+        print("An error occurred:", e)  
+
+ # Retrieve all the comment ratings from the database
+    mycursor = cnx.cursor()
+    mycursor.execute("SELECT csRate FROM comment")
+    results = mycursor.fetchall()
+
+    tot = 0
+    count = 0
+    for x in results: 
+       if x[0] is not None:
+          tot += x[0]
+          count += 1
+
+    if count > 0:
+       avg = tot / count
+       print("Average rating:", avg)
+
+    # Update the product table with the average rating
+       mycursor.execute("UPDATE product SET psRate = %s WHERE pId = %s", (avg, 2))
+       cnx.commit()
+    else:
+       print("No ratings found")
+         
+
+
+if __name__ == '__main__':
+    main()
--- Comment/submit.php
+++ Comment/submit.php
@@ -6,6 +6,16 @@
     if($comm != " "){
         $SQL="insert into comment (comm , pId) values ('$comm',  $id)";
         $exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error($conn));
+        $cId = mysqli_insert_id($conn);
+
+        // Execute the star_gen.py script with the $comm, $id, and $cId variables
+        $script = "python star_gen.py \"$comm\" \"$id\" \"$cId\"";
+        $output = shell_exec($script);
+
+        // Print the output of the Python script
+        echo $output;
         header('Location:../Product/buypage.php?pid='.$id);
+
+        
     }
 ?>
