361461817ced3e627821c44a3580ac860cb03b48
Hasitha20200655 <hasitha.20200655@iit.ac.lk>
2023-03-19 11:06:14 +0530

    Fixed the issue in comment writing
    issue when write a long comment it leads to a page with the id number

--- Comment/submit.php
+++ Comment/submit.php
@@ -2,20 +2,29 @@
     include("../Main/db.php");
     $id=$_POST['variable1'];
     $comm=$_POST['comment'];
+<<<<<<< Updated upstream
     echo "$id";
-    if($comm != " "){
-        $SQL="insert into comment (comm , pId) values ('$comm',  $id)";
-        $exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error($conn));
+    
+    if(isset($_POST['comment'])){
+        $SQL="INSERT INTO comment (comm , pId) VALUES ('".addslashes($comm)."',$id)";
+        //$exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error($conn));
+        //echo $SQL;
+        if (mysqli_query($conn, $SQL)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $SQL . "<br>" . mysqli_error($conn);
+        }
         $cId = mysqli_insert_id($conn);
-
 
         // // Execute the star_gen.py script with the $comm, $id, and $cId variables
         $script = "python ../Python_backend/model.py";
         $output = shell_exec($script);
-        
 
         // Print the output of the Python script
         echo $output;
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
         header('Location:../Product/buypage.php?pid='.$id);
     }
+=======
 ?>
